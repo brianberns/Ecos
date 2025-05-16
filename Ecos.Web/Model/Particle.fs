17aad072c6a348b147edd6ce89dfaf04062e4252
Brian Berns <brianberns@gmail.com>
2025-05-16 09:12:19 -0400

    Modularized.

---
+++ Ecos.Web/Model/Particle.fs
@@ -0,0 +1,28 @@
+﻿namespace Ecos
+
+type Particle =
+    {
+        Valence : int
+        NumBonds : int
+        Location : Point
+    }
+
+module Particle =
+
+    let create valence location =
+        {
+            Valence = valence
+            NumBonds = 0
+            Location = location
+        }
+
+    let resetBonds particle =
+        { particle with NumBonds = 0 }
+
+    let bond particleA particleB =
+        assert(particleA.NumBonds < particleA.Valence)
+        assert(particleB.NumBonds < particleB.Valence)
+        { particleA with
+            NumBonds = particleA.NumBonds + 1 },
+        { particleB with
+            NumBonds = particleB.NumBonds + 1 }
--- Ecos.Web/Model/World.fs
+++ Ecos.Web/Model/World.fs
@@ -2,33 +2,6 @@
 
 open System
 
-type Particle =
-    {
-        Valence : int
-        NumBonds : int
-        Location : Point
-    }
-
-module Particle =
-
-    let create valence location =
-        {
-            Valence = valence
-            NumBonds = 0
-            Location = location
-        }
-
-    let resetBonds particle =
-        { particle with NumBonds = 0 }
-
-    let bond particleA particleB =
-        assert(particleA.NumBonds < particleA.Valence)
-        assert(particleB.NumBonds < particleB.Valence)
-        { particleA with
-            NumBonds = particleA.NumBonds + 1 },
-        { particleB with
-            NumBonds = particleB.NumBonds + 1 }
-
 /// World of objects to animate.
 type World =
     {
