df66cf9540bb6eb09c089e9b3315fef9f5c73fa6
Brian Berns <brianberns@gmail.com>
2025-05-19 23:55:35 -0400

    Commented.

--- Ecos.Web/Model/ImmutableArray.fs
+++ Ecos.Web/Model/ImmutableArray.fs
@@ -1,19 +1,25 @@
 ﻿namespace Ecos
 
+/// Immutable array.
 type ImmutableArray<'t> =
     private {
         Items : 't[]
     }
 
+    /// Creates an immutable array.
     static member Create(items) =
         { Items = [| yield! items |] }
 
+    /// Accesses this array at the given index.
     member this.Item
         with get(i) = this.Items[i]
 
+    /// Number of items in this array.
     member this.Length =
         this.Items.Length
 
+    /// Creates a new array with the given item at the given
+    // index.
     member this.SetItem(idx, item) =
         {
             Items =
